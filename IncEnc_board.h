d960831eb4bd2af012e70cd692ac38a81fa5ff65
Shaoryu <149553583+Shaoryu@users.noreply.github.com>
2026-07-15 17:01:25 +0900

    Refactor IncEnc_board class to use mbed namespace

--- IncEnc_board.h
+++ IncEnc_board.h
@@ -4,7 +4,7 @@
 #include "CANManager.h"
 class IncEnc_board :public CANReceiver{
 public:
-    IncEnc_board(CAN &can, int all_node_num);
+    IncEnc_board(mbed::CAN &can, int all_node_num);
     bool encoder_reset_node(int node);
     bool encoder_reset_all();
     void conv_data_node(int64_t* angle, uint8_t node);
@@ -12,11 +12,11 @@ public:
     
 
 private:
-    bool handle_message(const CANMessage &msg) override;
+    bool handle_message(const mbed::CANMessage &msg) override;
     //void data_control();
     int _all_node_num;
-    CAN &_can;
-    Mutex _data_mutex;
+    mbed::CAN &_can;
+    rtos::Mutex _data_mutex;
     std::vector<CANMessage> _msg_buffer;
     uint8_t _new_data_mask;
 };
